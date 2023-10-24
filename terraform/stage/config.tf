ca9b9d332fcee1b154e4c236f15617501e2928c5
root <root@tao.localdomain>
2023-10-25 00:40:58 +0500

    change file=config.tf for terraform-2 v.02

--- terraform/stage/config.tf
+++ terraform/stage/config.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     yandex = {
       source  = "yandex-cloud/yandex"
-      version = "~>0.100"
+      version = "0.95.0"
     }
   }
   required_version = "~>1.5.1"
